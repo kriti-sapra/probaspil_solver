7cdf61117787aea2aa0793a5575e740232f0630a
Kriti Sapra <sapra.kriti@gmail.com>
2022-05-11 17:11:42 +0100

    [Runs]: Uploading results of experiments

--- iterative_prob_aspal_with_problog.py
+++ iterative_prob_aspal_with_problog.py
@@ -14,7 +14,7 @@ import argparse
 
 
 DEFAULT_FILE = 'experiments/smokes.lp'
-BASE_PATH = '/Users/kritisapra/Desktop/Imperial/Fourth_Year/prob_aspal'
+BASE_PATH = '/home/kriti/Desktop/FYP/prob_aspal_solver/'
 LOG_FILENAME = BASE_PATH + '/tmp/aspal.log'
 
 ERROR_LOG_FILE = BASE_PATH + '/tmp/problog.log'

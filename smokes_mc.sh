31303f7da59f1003ea96c88fd36d0b995edf4f44
Kriti Sapra <sapra.kriti@gmail.com>
2022-05-11 22:15:16 +0100

    [Experiments]: Updated Scripts to run on larger files

--- smokes_mc.sh
+++ smokes_mc.sh
@@ -7,51 +7,51 @@ conda activate potassco
 echo "Experiment 1 \n" > results/smokes_mc.txt
 echo "Experiment 1 \n"
 
-python prob_aspal_with_constants.py -f experiments/smokes.lp -mc 1 >> results/smokes_mc.txt
+python prob_aspal_with_constants.py -f experiments/surf.lp -mc 1 >> results/smokes_mc.txt
 
 echo "Experiment 2 \n" >> results/smokes_mc.txt
 echo "Experiment 2 \n"
 
-python prob_aspal_with_constants.py -f experiments/smokes.lp -mc 2 >> results/smokes_mc.txt
+python prob_aspal_with_constants.py -f experiments/surf.lp -mc 2 >> results/smokes_mc.txt
 
 echo "Experiment 3 \n" >> results/smokes_mc.txt
 echo "Experiment 3 \n"
 
-python prob_aspal_with_constants.py -f experiments/smokes.lp -mc 3 >> results/smokes_mc.txt
+python prob_aspal_with_constants.py -f experiments/surf.lp -mc 3 >> results/smokes_mc.txt
 
 echo "Experiment 4 \n" >> results/smokes_mc.txt
 echo "Experiment 4 \n"
 
-python prob_aspal_with_constants.py -f experiments/smokes.lp -mc 4 >> results/smokes_mc.txt
+python prob_aspal_with_constants.py -f experiments/surf.lp -mc 4 >> results/smokes_mc.txt
 
 echo "Experiment 5 \n" >> results/smokes_mc.txt
 echo "Experiment 5 \n"
 
-python prob_aspal_with_constants.py -f experiments/smokes.lp -mc 5 >> results/smokes_mc.txt
+python prob_aspal_with_constants.py -f experiments/surf.lp -mc 5 >> results/smokes_mc.txt
 
 echo "Experiment 6 \n" >> results/smokes_mc.txt
 echo "Experiment 6 \n"
 
-python prob_aspal_with_constants.py -f experiments/smokes.lp -mc 6 >> results/smokes_mc.txt
+python prob_aspal_with_constants.py -f experiments/surf.lp -mc 6 >> results/smokes_mc.txt
 
 echo "Experiment 7 \n" >> results/smokes_mc.txt
 echo "Experiment 7 \n"
 
-python prob_aspal_with_constants.py -f experiments/smokes.lp -mc 7 >> results/smokes_mc.txt
+python prob_aspal_with_constants.py -f experiments/surf.lp -mc 7 >> results/smokes_mc.txt
 
 echo "Experiment 8 \n" >> results/smokes_mc.txt
 echo "Experiment 8 \n"
 
-python prob_aspal_with_constants.py -f experiments/smokes.lp -mc 8 >> results/smokes_mc.txt
+python prob_aspal_with_constants.py -f experiments/surf.lp -mc 8 >> results/smokes_mc.txt
 
 echo "Experiment 9 \n" >> results/smokes_mc.txt
 echo "Experiment 9 \n"
 
-python prob_aspal_with_constants.py -f experiments/smokes.lp -mc 9 >> results/smokes_mc.txt
+python prob_aspal_with_constants.py -f experiments/surf.lp -mc 9 >> results/smokes_mc.txt
 
 echo "Experiment 10 \n" >> results/smokes_mc.txt
 echo "Experiment 10 \n"
 
-python prob_aspal_with_constants.py -f experiments/smokes.lp -mc 10 >> results/smokes_mc.txt
+python prob_aspal_with_constants.py -f experiments/surf.lp -mc 10 >> results/smokes_mc.txt
 
 echo "Finished running experiments. See results in results/smokes_mc.txt"
--- smokes_problog_mr.sh
+++ smokes_problog_mr.sh
@@ -7,51 +7,51 @@ conda activate potassco
 echo "Experiment 1 \n" > results/smokes_problog_mr.txt
 echo "Experiment 1 \n"
 
-python prob_aspal_with_problog.py -f experiments/smokes.lp -mr 1 >> results/smokes_problog_mr.txt
+python prob_aspal_with_problog.py -f experiments/surf.lp -mr 1 >> results/smokes_problog_mr.txt
 
 echo "Experiment 2 \n" >> results/smokes_problog_mr.txt
 echo "Experiment 2 \n"
 
-python prob_aspal_with_problog.py -f experiments/smokes.lp -mr 2 >> results/smokes_problog_mr.txt
+python prob_aspal_with_problog.py -f experiments/surf.lp -mr 2 >> results/smokes_problog_mr.txt
 
 echo "Experiment 3 \n" >> results/smokes_problog_mr.txt
 echo "Experiment 3 \n"
 
-python prob_aspal_with_problog.py -f experiments/smokes.lp -mr 3 >> results/smokes_problog_mr.txt
+python prob_aspal_with_problog.py -f experiments/surf.lp -mr 3 >> results/smokes_problog_mr.txt
 
 echo "Experiment 4 \n" >> results/smokes_problog_mr.txt
 echo "Experiment 4 \n"
 
-python prob_aspal_with_problog.py -f experiments/smokes.lp -mr 4 >> results/smokes_problog_mr.txt
+python prob_aspal_with_problog.py -f experiments/surf.lp -mr 4 >> results/smokes_problog_mr.txt
 
 echo "Experiment 5 \n" >> results/smokes_problog_mr.txt
 echo "Experiment 5 \n"
 
-python prob_aspal_with_problog.py -f experiments/smokes.lp -mr 5 >> results/smokes_problog_mr.txt
+python prob_aspal_with_problog.py -f experiments/surf.lp -mr 5 >> results/smokes_problog_mr.txt
 
 echo "Experiment 6 \n" >> results/smokes_problog_mr.txt
 echo "Experiment 6 \n"
 
-python prob_aspal_with_problog.py -f experiments/smokes.lp -mr 6 >> results/smokes_problog_mr.txt
+python prob_aspal_with_problog.py -f experiments/surf.lp -mr 6 >> results/smokes_problog_mr.txt
 
 echo "Experiment 7 \n" >> results/smokes_problog_mr.txt
 echo "Experiment 7 \n"
 
-python prob_aspal_with_problog.py -f experiments/smokes.lp -mr 7 >> results/smokes_problog_mr.txt
+python prob_aspal_with_problog.py -f experiments/surf.lp -mr 7 >> results/smokes_problog_mr.txt
 
 echo "Experiment 8 \n" >> results/smokes_problog_mr.txt
 echo "Experiment 8 \n"
 
-python prob_aspal_with_problog.py -f experiments/smokes.lp -mr 8 >> results/smokes_problog_mr.txt
+python prob_aspal_with_problog.py -f experiments/surf.lp -mr 8 >> results/smokes_problog_mr.txt
 
 echo "Experiment 9 \n" >> results/smokes_problog_mr.txt
 echo "Experiment 9 \n"
 
-python prob_aspal_with_problog.py -f experiments/smokes.lp -mr 9 >> results/smokes_problog_mr.txt
+python prob_aspal_with_problog.py -f experiments/surf.lp -mr 9 >> results/smokes_problog_mr.txt
 
 echo "Experiment 10 \n" >> results/smokes_problog_mr.txt
 echo "Experiment 10 \n"
 
-python prob_aspal_with_problog.py -f experiments/smokes.lp -mr 10 >> results/smokes_problog_mr.txt
+python prob_aspal_with_problog.py -f experiments/surf.lp -mr 10 >> results/smokes_problog_mr.txt
 
 echo "Finished running experiments. See results in results/smokes_problog_mr.txt"

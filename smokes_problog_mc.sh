4797c152370c06c6eb45eb7427d83a1a9c1e43b9
Kriti Sapra <sapra.kriti@gmail.com>
2022-05-12 10:43:15 +0100

    [Experiments]: Updated file to save to results/surf_

--- smokes_problog_mc.sh
+++ smokes_problog_mc.sh
@@ -4,54 +4,54 @@ source /home/kriti/anaconda3/etc/profile.d/conda.sh
 
 conda activate potassco
 
-echo "Experiment 1 \n" > results/smokes_problog_mc.txt
+echo "Experiment 1 \n" > results/surf_problog_mc.txt
 echo "Experiment 1 \n"
 
-python prob_aspal_with_problog.py -f experiments/surf.lp -mc 1 >> results/smokes_problog_mc.txt
+python prob_aspal_with_problog.py -f experiments/surf.lp -mc 1 >> results/surf_problog_mc.txt
 
-echo "Experiment 2 \n" >> results/smokes_problog_mc.txt
+echo "Experiment 2 \n" >> results/surf_problog_mc.txt
 echo "Experiment 2 \n"
 
-python prob_aspal_with_problog.py -f experiments/surf.lp -mc 2 >> results/smokes_problog_mc.txt
+python prob_aspal_with_problog.py -f experiments/surf.lp -mc 2 >> results/surf_problog_mc.txt
 
-echo "Experiment 3 \n" >> results/smokes_problog_mc.txt
+echo "Experiment 3 \n" >> results/surf_problog_mc.txt
 echo "Experiment 3 \n"
 
-python prob_aspal_with_problog.py -f experiments/surf.lp -mc 3 >> results/smokes_problog_mc.txt
+python prob_aspal_with_problog.py -f experiments/surf.lp -mc 3 >> results/surf_problog_mc.txt
 
-echo "Experiment 4 \n" >> results/smokes_problog_mc.txt
+echo "Experiment 4 \n" >> results/surf_problog_mc.txt
 echo "Experiment 4 \n"
 
-python prob_aspal_with_problog.py -f experiments/surf.lp -mc 4 >> results/smokes_problog_mc.txt
+python prob_aspal_with_problog.py -f experiments/surf.lp -mc 4 >> results/surf_problog_mc.txt
 
-echo "Experiment 5 \n" >> results/smokes_problog_mc.txt
+echo "Experiment 5 \n" >> results/surf_problog_mc.txt
 echo "Experiment 5 \n"
 
-python prob_aspal_with_problog.py -f experiments/surf.lp -mc 5 >> results/smokes_problog_mc.txt
+python prob_aspal_with_problog.py -f experiments/surf.lp -mc 5 >> results/surf_problog_mc.txt
 
-echo "Experiment 6 \n" >> results/smokes_problog_mc.txt
+echo "Experiment 6 \n" >> results/surf_problog_mc.txt
 echo "Experiment 6 \n"
 
-python prob_aspal_with_problog.py -f experiments/surf.lp -mc 6 >> results/smokes_problog_mc.txt
+python prob_aspal_with_problog.py -f experiments/surf.lp -mc 6 >> results/surf_problog_mc.txt
 
-echo "Experiment 7 \n" >> results/smokes_problog_mc.txt
+echo "Experiment 7 \n" >> results/surf_problog_mc.txt
 echo "Experiment 7 \n"
 
-python prob_aspal_with_problog.py -f experiments/surf.lp -mc 7 >> results/smokes_problog_mc.txt
+python prob_aspal_with_problog.py -f experiments/surf.lp -mc 7 >> results/surf_problog_mc.txt
 
-echo "Experiment 8 \n" >> results/smokes_problog_mc.txt
+echo "Experiment 8 \n" >> results/surf_problog_mc.txt
 echo "Experiment 8 \n"
 
-python prob_aspal_with_problog.py -f experiments/surf.lp -mc 8 >> results/smokes_problog_mc.txt
+python prob_aspal_with_problog.py -f experiments/surf.lp -mc 8 >> results/surf_problog_mc.txt
 
-echo "Experiment 9 \n" >> results/smokes_problog_mc.txt
+echo "Experiment 9 \n" >> results/surf_problog_mc.txt
 echo "Experiment 9 \n"
 
-python prob_aspal_with_problog.py -f experiments/surf.lp -mc 9 >> results/smokes_problog_mc.txt
+python prob_aspal_with_problog.py -f experiments/surf.lp -mc 9 >> results/surf_problog_mc.txt
 
-echo "Experiment 10 \n" >> results/smokes_problog_mc.txt
+echo "Experiment 10 \n" >> results/surf_problog_mc.txt
 echo "Experiment 10 \n"
 
-python prob_aspal_with_problog.py -f experiments/surf.lp -mc 10 >> results/smokes_problog_mc.txt
+python prob_aspal_with_problog.py -f experiments/surf.lp -mc 10 >> results/surf_problog_mc.txt
 
-echo "Finished running experiments. See results in results/smokes_problog_mc.txt"
+echo "Finished running experiments. See results in results/surf_problog_mc.txt"

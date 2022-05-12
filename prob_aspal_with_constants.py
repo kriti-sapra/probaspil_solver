d2ce0d3067c257a2cb9756940d8f993dc78f7dd9
Kriti Sapra <sapra.kriti@gmail.com>
2022-05-12 12:36:49 +0100

    [Experiments]: Ran alpha and beta experiments on different h size

--- prob_aspal_with_constants.py
+++ prob_aspal_with_constants.py
@@ -11,7 +11,7 @@ import sys
 import argparse
 
 DEFAULT_FILE = 'experiments/smokes.lp'
-BASE_PATH = '/Users/kritisapra/Desktop/Imperial/Fourth_Year/prob_aspal/'
+BASE_PATH = '/home/kriti/Desktop/FYP/prob_aspal_solver/'
 LOG_FILENAME = BASE_PATH + '/tmp/aspal.log'
 
 SOLVER = ''

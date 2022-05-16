e5d31fee071abac1053bbfd7ef01f6f1856dcfed
Kriti Sapra <sapra.kriti@gmail.com>
2022-05-16 14:13:31 +0100

    [Experiments]: Alpha Beta with max rules 2

--- smokes_beta_version.sh
+++ smokes_beta_version.sh
@@ -7,56 +7,56 @@ conda activate potassco
 echo "Experiment 1 \n" > results/smokes_beta_scores.txt
 echo "Experiment 1 \n"
 
-python prob_aspal_with_constants.py -f experiments/smokes.lp -b 0 >> results/smokes_beta_scores.txt
+python prob_aspal_with_constants.py -f experiments/smokes.lp -mr 2 -b 0 >> results/smokes_beta_scores.txt
 
 echo "Experiment 2 \n" >> results/smokes_beta_scores.txt
 echo "Experiment 2 \n"
 
-python prob_aspal_with_constants.py -f experiments/smokes.lp -b 0.1 >> results/smokes_beta_scores.txt
+python prob_aspal_with_constants.py -f experiments/smokes.lp -mr 2 -b 0.1 >> results/smokes_beta_scores.txt
 
 echo "Experiment 3 \n" >> results/smokes_beta_scores.txt
 echo "Experiment 3 \n"
 
-python prob_aspal_with_constants.py -f experiments/smokes.lp -b 0.2 >> results/smokes_beta_scores.txt
+python prob_aspal_with_constants.py -f experiments/smokes.lp -mr 2 -b 0.2 >> results/smokes_beta_scores.txt
 
 echo "Experiment 4 \n" >> results/smokes_beta_scores.txt
 echo "Experiment 4 \n"
 
-python prob_aspal_with_constants.py -f experiments/smokes.lp -b 0.3 >> results/smokes_beta_scores.txt
+python prob_aspal_with_constants.py -f experiments/smokes.lp -mr 2 -b 0.3 >> results/smokes_beta_scores.txt
 
 echo "Experiment 5 \n" >> results/smokes_beta_scores.txt
 echo "Experiment 5 \n"
 
-python prob_aspal_with_constants.py -f experiments/smokes.lp -b 0.4 >> results/smokes_beta_scores.txt
+python prob_aspal_with_constants.py -f experiments/smokes.lp -mr 2 -b 0.4 >> results/smokes_beta_scores.txt
 
 echo "Experiment 6 \n" >> results/smokes_beta_scores.txt
 echo "Experiment 6 \n"
 
-python prob_aspal_with_constants.py -f experiments/smokes.lp -b 0.5 >> results/smokes_beta_scores.txt
+python prob_aspal_with_constants.py -f experiments/smokes.lp -mr 2 -b 0.5 >> results/smokes_beta_scores.txt
 
 echo "Experiment 7 \n" >> results/smokes_beta_scores.txt
 echo "Experiment 7 \n"
 
-python prob_aspal_with_constants.py -f experiments/smokes.lp -b 0.6 >> results/smokes_beta_scores.txt
+python prob_aspal_with_constants.py -f experiments/smokes.lp -mr 2 -b 0.6 >> results/smokes_beta_scores.txt
 
 echo "Experiment 8 \n" >> results/smokes_beta_scores.txt
 echo "Experiment 8 \n"
 
-python prob_aspal_with_constants.py -f experiments/smokes.lp -b 0.7 >> results/smokes_beta_scores.txt
+python prob_aspal_with_constants.py -f experiments/smokes.lp -mr 2 -b 0.7 >> results/smokes_beta_scores.txt
 
 echo "Experiment 9 \n" >> results/smokes_beta_scores.txt
 echo "Experiment 9 \n"
 
-python prob_aspal_with_constants.py -f experiments/smokes.lp -b 0.8 >> results/smokes_beta_scores.txt
+python prob_aspal_with_constants.py -f experiments/smokes.lp -mr 2 -b 0.8 >> results/smokes_beta_scores.txt
 
 echo "Experiment 10 \n" >> results/smokes_beta_scores.txt
 echo "Experiment 10 \n"
 
-python prob_aspal_with_constants.py -f experiments/smokes.lp -b 0.9 >> results/smokes_beta_scores.txt
+python prob_aspal_with_constants.py -f experiments/smokes.lp -mr 2 -b 0.9 >> results/smokes_beta_scores.txt
 
 echo "Experiment 11 \n" >> results/smokes_beta_scores.txt
 echo "Experiment 11 \n"
 
-python prob_aspal_with_constants.py -f experiments/smokes.lp -b 1 >> results/smokes_beta_scores.txt
+python prob_aspal_with_constants.py -f experiments/smokes.lp -mr 2 -b 1 >> results/smokes_beta_scores.txt
 
 echo "Finished running experiments. See results in results/smokes_beta_scores.txt"

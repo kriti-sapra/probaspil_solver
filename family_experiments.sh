9cbc4408e4ee8402042ff2edae70d7c5cb821b60
Kriti Sapra <sapra.kriti@gmail.com>
2022-05-23 18:24:21 +0100

    [Experiments]: More multiple AS

--- family_experiments.sh
+++ family_experiments.sh
@@ -7,19 +7,19 @@ conda activate potassco
 echo "Experiment 1 Prob ASPAL\n" >> results/family_experiments.txt
 echo "Experiment 1 Prob ASPAL\n"
 
-python prob_aspal_with_constants.py -f experiments/family.lp  >> results/family_experiments.txt
+#python prob_aspal_with_constants.py -f experiments/family.lp  >> results/family_experiments.txt
 
 echo "Experiment 2 Early Stop Prob ASPAL\n" >> results/family_experiments.txt
 echo "Experiment 2 Early Stop Prob ASPAL\n"
 
-python early_stop_prob_aspal.py -f experiments/family.lp  >> results/family_experiments.txt
+#python early_stop_prob_aspal.py -f experiments/family.lp  >> results/family_experiments.txt
 
-echo "Experiment 3 Prob ASPAL with ProbLog\n" >> results/family_experiments.txt
-echo "Experiment 3 Prob ASPAL with ProbLog\n"
-
-python prob_aspal_with_problog.py -f experiments/family.lp  >> results/family_experiments.txt
-
-echo "Experiment 4 Early Stop Prob ASPAL with ProbLog\n" >> results/family_experiments.txt
-echo "Experiment 4 Early Stop Prob ASPAL with ProbLog\n"
+echo "Experiment 3 EArly Stop Prob ASPAL with ProbLog\n" >> results/family_experiments.txt
+echo "Experiment 3 Early STop Prob ASPAL with ProbLog\n"
 
 python early_stop_prob_aspal_with_problog.py -f experiments/family.lp  >> results/family_experiments.txt
+
+echo "Experiment 4 Prob ASPAL with ProbLog\n" >> results/family_experiments.txt
+echo "Experiment 4 Prob ASPAL with ProbLog\n"
+
+python prob_aspal_with_problog.py -f experiments/family.lp  >> results/family_experiments.txt

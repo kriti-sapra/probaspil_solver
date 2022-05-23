405ee6ab725a2b3c427995802e03c03814af64d2
Kriti Sapra <sapra.kriti@gmail.com>
2022-05-23 13:39:44 +0100

    [Experiments]: Scripts for comparing multiple answer sets to single answer sets via time and also for increasing facts in family example

---
+++ smokes_multiple_answer_sets.sh
@@ -0,0 +1,35 @@
+echo "Starting Experiments \n"
+
+source /home/kriti/anaconda3/etc/profile.d/conda.sh
+
+conda activate potassco
+
+echo "Experiment 1 Prob ASPAL\n" > results/smokes_multiple_as.txt
+echo "Experiment 1 Prob ASPAL\n"
+
+python prob_aspal_with_constants.py -f experiments/smokes.lp  >> results/smokes_multiple_as.txt
+
+echo "Experiment 2 Early Stop Prob ASPAL\n" > results/smokes_multiple_as.txt
+echo "Experiment 2 Early Stop Prob ASPAL\n"
+
+python early_stop_prob_aspal_with_constants.py -f experiments/smokes.lp  >> results/smokes_multiple_as.txt
+
+echo "Experiment 3 Prob ASPAL with multiple answer sets\n" > results/smokes_multiple_as.txt
+echo "Experiment 3 Prob ASPAL with multiple answer sets\n"
+
+python prob_aspal_multiple_answersets.py -f experiments/smokes.lp  >> results/smokes_multiple_as.txt
+
+echo "Experiment 4 Early Stop Prob ASPAL with multiple answer sets\n" > results/smokes_multiple_as.txt
+echo "Experiment 4 Early Stop Prob ASPAL with multiple answer sets\n"
+
+python early_stop_prob_aspal_with_problog.py -f experiments/smokes.lp  >> results/smokes_multiple_as.txt
+
+echo "Experiment 5 Prob ASPAL with credal\n" > results/smokes_multiple_as.txt
+echo "Experiment 5 Prob ASPAL with credal\n"
+
+python credal_semantics_prob_aspal.py -f experiments/credal_smokes.lp  >> results/smokes_multiple_as.txt
+
+echo "Experiment 6 Early Stop Prob ASPAL with credal\n" > results/smokes_multiple_as.txt
+echo "Experiment 6 Early Stop Prob ASPAL with credal\n"
+
+python early_stop_credal_semantics.py -f experiments/credal_smokes.lp  >> results/smokes_multiple_as.txt

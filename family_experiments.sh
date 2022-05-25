df56a6f47a8baa21dd4cc474121f7a6aca77fcf8
Kriti Sapra <sapra.kriti@gmail.com>
2022-05-25 11:06:38 +0100

    [Experiments]: Added 2 more examples to family.lp

--- family_experiments.sh
+++ family_experiments.sh
@@ -4,20 +4,21 @@ source /home/kriti/anaconda3/etc/profile.d/conda.sh
 
 conda activate potassco
 
-echo "Experiment 1 Prob ASPAL\n" > results/family_experiments.txt
-echo "Experiment 1 Prob ASPAL\n"
-
-python prob_aspal_with_constants.py -f experiments/family.lp -e 0.5 >> results/family_experiments.txt
-
-echo "Experiment 2 Early Stop Prob ASPAL\n" >> results/family_experiments.txt
-echo "Experiment 2 Early Stop Prob ASPAL\n"
+echo "Experiment 1 Early Stop Prob ASPAL\n" >> results/family_experiments.txt
+echo "Experiment 1 Early Stop Prob ASPAL\n"
 
 python early_stop_prob_aspal.py -f experiments/family.lp  >> results/family_experiments.txt
 
-echo "Experiment 3 EArly Stop Prob ASPAL with ProbLog\n" >> results/family_experiments.txt
-echo "Experiment 3 Early STop Prob ASPAL with ProbLog\n"
+echo "Experiment 2 EArly Stop Prob ASPAL with ProbLog\n" >> results/family_experiments.txt
+echo "Experiment 2 Early STop Prob ASPAL with ProbLog\n"
 
 python early_stop_prob_aspal_with_problog.py -f experiments/family.lp  >> results/family_experiments.txt
+
+
+echo "Experiment 3 Prob ASPAL\n" > results/family_experiments.txt
+echo "Experiment 3 Prob ASPAL\n"
+
+python prob_aspal_with_constants.py -f experiments/family.lp -e 0.5 >> results/family_experiments.txt
 
 #echo "Experiment 4 Prob ASPAL with ProbLog\n" >> results/family_experiments.txt
 #echo "Experiment 4 Prob ASPAL with ProbLog\n"

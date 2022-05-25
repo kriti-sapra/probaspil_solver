008642e81a47fff0f906500b0cb9622c819a5fbb
Kriti Sapra <sapra.kriti@gmail.com>
2022-05-25 14:36:04 +0100

    [Experiments]: More multiple AS

--- family_experiments.sh
+++ family_experiments.sh
@@ -4,7 +4,7 @@ source /home/kriti/anaconda3/etc/profile.d/conda.sh
 
 conda activate potassco
 
-echo "Experiment 1 Early Stop Prob ASPAL\n" >> results/family_experiments.txt
+echo "Experiment 1 Early Stop Prob ASPAL\n" > results/family_experiments.txt
 echo "Experiment 1 Early Stop Prob ASPAL\n"
 
 python early_stop_prob_aspal.py -f experiments/family.lp  >> results/family_experiments.txt
@@ -18,7 +18,7 @@ python early_stop_prob_aspal_with_problog.py -f experiments/family.lp  >> result
 echo "Experiment 3 Prob ASPAL\n" > results/family_experiments.txt
 echo "Experiment 3 Prob ASPAL\n"
 
-python prob_aspal_with_constants.py -f experiments/family.lp -e 0.5 >> results/family_experiments.txt
+#python prob_aspal_with_constants.py -f experiments/family.lp -e 0.5 >> results/family_experiments.txt
 
 #echo "Experiment 4 Prob ASPAL with ProbLog\n" >> results/family_experiments.txt
 #echo "Experiment 4 Prob ASPAL with ProbLog\n"

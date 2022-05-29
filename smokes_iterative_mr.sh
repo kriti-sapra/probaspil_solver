0c0566aca50d0718f08506a196537ba3d83449b4
Kriti Sapra <sapra.kriti@gmail.com>
2022-05-29 13:46:25 +0100

    [Experiments]: Running smokes experiments again

--- smokes_iterative_mr.sh
+++ smokes_iterative_mr.sh
@@ -4,54 +4,54 @@ source /home/kriti/anaconda3/etc/profile.d/conda.sh
 
 conda activate potassco
 
-echo "Experiment 1 \n" > results/surf_iterative_mr.txt
+echo "Experiment 1 \n" > results/smokes_early_stop_mr.txt
 echo "Experiment 1 \n"
 
-python iterative_prob_aspal.py -f experiments/surf.lp -mr 1 >> results/surf_iterative_mr.txt
+python early_stop_prob_aspal.py -f experiments/smokes.lp -mr 1 >> results/smokes_early_stop_mr.txt
 
-echo "Experiment 2 \n" >> results/surf_iterative_mr.txt
+echo "Experiment 2 \n" >> results/smokes_early_stop_mr.txt
 echo "Experiment 2 \n"
 
-python iterative_prob_aspal.py -f experiments/surf.lp -mr 2 >> results/surf_iterative_mr.txt
+python early_stop_prob_aspal.py -f experiments/smokes.lp -mr 2 >> results/smokes_early_stop_mr.txt
 
-echo "Experiment 3 \n" >> results/surf_iterative_mr.txt
+echo "Experiment 3 \n" >> results/smokes_early_stop_mr.txt
 echo "Experiment 3 \n"
 
-python iterative_prob_aspal.py -f experiments/surf.lp -mr 3 >> results/surf_iterative_mr.txt
+python early_stop_prob_aspal.py -f experiments/smokes.lp -mr 3 >> results/smokes_early_stop_mr.txt
 
-echo "Experiment 4 \n" >> results/surf_iterative_mr.txt
+echo "Experiment 4 \n" >> results/smokes_early_stop_mr.txt
 echo "Experiment 4 \n"
 
-python iterative_prob_aspal.py -f experiments/surf.lp -mr 4 >> results/surf_iterative_mr.txt
+python early_stop_prob_aspal.py -f experiments/smokes.lp -mr 4 >> results/smokes_early_stop_mr.txt
 
-echo "Experiment 5 \n" >> results/surf_iterative_mr.txt
+echo "Experiment 5 \n" >> results/smokes_early_stop_mr.txt
 echo "Experiment 5 \n"
 
-python iterative_prob_aspal.py -f experiments/surf.lp -mr 5 >> results/surf_iterative_mr.txt
+python early_stop_prob_aspal.py -f experiments/smokes.lp -mr 5 >> results/smokes_early_stop_mr.txt
 
-echo "Experiment 6 \n" >> results/surf_iterative_mr.txt
+echo "Experiment 6 \n" >> results/smokes_early_stop_mr.txt
 echo "Experiment 6 \n"
 
-python iterative_prob_aspal.py -f experiments/surf.lp -mr 6 >> results/surf_iterative_mr.txt
+python early_stop_prob_aspal.py -f experiments/smokes.lp -mr 6 >> results/smokes_early_stop_mr.txt
 
-echo "Experiment 7 \n" >> results/surf_iterative_mr.txt
+echo "Experiment 7 \n" >> results/smokes_early_stop_mr.txt
 echo "Experiment 7 \n"
 
-python iterative_prob_aspal.py -f experiments/surf.lp -mr 7 >> results/surf_iterative_mr.txt
+python early_stop_prob_aspal.py -f experiments/smokes.lp -mr 7 >> results/smokes_early_stop_mr.txt
 
-echo "Experiment 8 \n" >> results/surf_iterative_mr.txt
+echo "Experiment 8 \n" >> results/smokes_early_stop_mr.txt
 echo "Experiment 8 \n"
 
-python iterative_prob_aspal.py -f experiments/surf.lp -mr 8 >> results/surf_iterative_mr.txt
+python early_stop_prob_aspal.py -f experiments/smokes.lp -mr 8 >> results/smokes_early_stop_mr.txt
 
-echo "Experiment 9 \n" >> results/surf_iterative_mr.txt
+echo "Experiment 9 \n" >> results/smokes_early_stop_mr.txt
 echo "Experiment 9 \n"
 
-python iterative_prob_aspal.py -f experiments/surf.lp -mr 9 >> results/surf_iterative_mr.txt
+python early_stop_prob_aspal.py -f experiments/smokes.lp -mr 9 >> results/smokes_early_stop_mr.txt
 
-echo "Experiment 10 \n" >> results/surf_iterative_mr.txt
+echo "Experiment 10 \n" >> results/smokes_early_stop_mr.txt
 echo "Experiment 10 \n"
 
-python iterative_prob_aspal.py -f experiments/surf.lp -mr 10 >> results/surf_iterative_mr.txt
+python early_stop_prob_aspal.py -f experiments/smokes.lp -mr 10 >> results/smokes_early_stop_mr.txt
 
-echo "Finished running experiments. See results in results/surf_iterative_mr.txt"
+echo "Finished running experiments. See results in results/smokes_early_stop_mr.txt"
